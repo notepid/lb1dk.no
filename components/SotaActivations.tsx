38c6606f043c9097ebb63b2a0c20546a5f4f9cf5
Notepid <1101091+notepid@users.noreply.github.com>
2024-09-28 09:06:33 +0200

    * Again making prettier happy again. If this persists then prettier gets removed...

--- components/SotaActivations.tsx
+++ components/SotaActivations.tsx
@@ -1,15 +1,15 @@
 export default function SotaActivations({ numActivations = 5 }) {
-    return (
-        <div className="mb-8">
-            <h2 className="text-lg font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10 md:text-2xl md:leading-14">
-                Last {numActivations} SOTA activations
-            </h2>
+  return (
+    <div className="mb-8">
+      <h2 className="text-lg font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10 md:text-2xl md:leading-14">
+        Last {numActivations} SOTA activations
+      </h2>
 
-            <iframe
-                className="w-full h-44"
-                src={`https://neon1.net/sota/myact/?c=LB1DK&l=${numActivations}&w=1&h=1`}
-                title={`Last ${numActivations} SOTA activations`}
-            />
-        </div>
-    )
+      <iframe
+        className="h-44 w-full"
+        src={`https://neon1.net/sota/myact/?c=LB1DK&l=${numActivations}&w=1&h=1`}
+        title={`Last ${numActivations} SOTA activations`}
+      />
+    </div>
+  )
 }
